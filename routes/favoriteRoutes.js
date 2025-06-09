3aa4f84d0ad191515712254d470432403516097a
anhvo050605 <anhvo050605a@gmail.com>
2025-06-10 00:01:02 +0700

    a

--- routes/favoriteRoutes.js
+++ routes/favoriteRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const favoriteController = require('../controllers/favoriteController');
+const verifyToken = require('../middleware/verifyToken'); // ✅ import middleware
 
-router.post('/', favoriteController.addFavorite);
-router.delete('/', favoriteController.removeFavorite);
-router.get('/:userId', favoriteController.getFavoritesByUser);
+// Yêu cầu người dùng phải đăng nhập mới được thao tác
+router.post('/', verifyToken, favoriteController.addFavorite);
+router.delete('/', verifyToken, favoriteController.removeFavorite);
+router.get('/:userId', verifyToken, favoriteController.getFavoritesByUser);
 
 module.exports = router;
+
