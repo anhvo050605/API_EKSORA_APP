cdd52686cd5f7a3c8d9ef16a2ab161dbaa5201cb
anhvo050605 <anhvo050605a@gmail.com>
2025-07-09 00:31:08 +0700

    a

--- controllers/geminiController.js
+++ controllers/geminiController.js
@@ -6,7 +6,7 @@ const chatWithGemini = async (req, res) => {
     const { prompt } = req.body;
 
     const response = await axios.post(
-      "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent",
       {
         contents: [{ parts: [{ text: prompt }] }]
       },
