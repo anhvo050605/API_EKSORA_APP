d10ebb53769e61701112834adb68b08a5f114359
anhvo050605 <anhvo050605a@gmail.com>
2025-07-16 13:52:44 +0700

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -209,7 +209,7 @@ exports.cancelBooking = async (req, res) => {
     await Notification.create({
       user_id: booking.user_id,
       title: 'Huỷ tour thành công',
-      body: `Bạn đã huỷ tour ${booking.tour_title} thành công.`,
+      body: `Bạn đã huỷ tour ${booking.tour_title} thành công.`,  
       isRead: false,
     });
 
