1af5e1f1d577ceccc65213e211802beb59791998
anhvo050605 <anhvo050605a@gmail.com>
2025-07-23 23:55:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const adminRoutes = require('./routes/adminRoutes');
 
 //============================================================================================================
 var app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -100,7 +100,7 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
