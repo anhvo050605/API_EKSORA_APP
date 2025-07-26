9a0a8bac1fee8ec2baf3a19b4611bbcc854c97c7
anhvo050605 <anhvo050605a@gmail.com>
2025-07-27 00:32:12 +0700

    a

--- controllers/loginUserwithEmail.js
+++ controllers/loginUserwithEmail.js
@@ -25,7 +25,8 @@ const loginUserwithEmail = async (req, res) => {
     if (!user) {
       return res.status(400).json({ message: 'Email không tồn tại' });
     }
-
+    console.log("Password nhập:", password);
+    console.log("Password trong DB (hash):", user.password);
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: 'Mật khẩu không đúng' });
