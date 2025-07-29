aefb3ff0f369c468025b7d9339bafce566039215
anhvo050605 <anhvo050605a@gmail.com>
2025-07-29 23:38:25 +0700

    Update location_categoryRoutes.js

--- routes/location_categoryRoutes.js
+++ routes/location_categoryRoutes.js
@@ -6,6 +6,7 @@ const categoryController = require('../controllers/location_categoryController')
 router.get('/', categoryController.getAllCategories); 
 router.post('/', categoryController.createCategory); 
 router.get('/tours-by-location', categoryController.getToursByLocation); // Lấy tour theo danh mục hoặc 'all'
+router.get('/tours-by-location/all', categoryController.getAllToursByLocation);
 router.put('/:id', categoryController.updateCategory);     // ✅ cập nhật danh mục
 router.delete('/:id', categoryController.deleteCategory);
 module.exports = router;
